88f9d43588a896d2564d147c52e986f20c755f36
harshbrahmxatriya <55323345+harshbrahmxatriya@users.noreply.github.com>
2020-12-17 12:53:41 +0530

    backend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true,
+useCreateIndex: true}
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log("MongoDB database connection established successfully");
+})
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const exercisesRouter = require('/home/harsh/mern-excercise-tracker/backend/routes/exercises');
+const usersRouter = require('/home/harsh/mern-excercise-tracker/backend/routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+	console.log('Server is running on port: ${port}');
+});
